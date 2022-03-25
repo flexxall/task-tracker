83b3c68b6718775091a0a380dd9d9fa8bae827e3
= <=>
2022-03-25 07:29:27 -0500

    Button Component

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <button @click="onClick()" :style="{ background: color }" class="btn">{{ text }}</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+
+  export default defineComponent({
+    name: "Button",
+    props: {
+      text: String,
+      color: String,
+    },
+    methods: {
+      onClick() {
+        console.log("click");
+        
+      }
+    },
+  })
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button text="Add Task" color="green" />
+    <!-- <Button text="Update Task" color="burlywood"/> -->
+    <!-- <Button text="Delete Task" color="red"/> -->
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Button from "./Button.vue";
+
+export default defineComponent({
+  name: "Header",
+  // props: ["title"]
+  props: {
+    title: String,
+  },
+  // props: {
+  //   title: {
+  //     type: String,
+  //     default: "Hello World",
+  //   },
+  // },
+  components: {
+    Button,
+  },
+});
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
